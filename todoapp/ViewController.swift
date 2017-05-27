9d92d687b2d6e2fa0594cee4b1462a434a263c9f
qamarali <qamarali@qamaralis-MacBook-Pro.local>
2017-05-26 21:34:26 -0500

    Initial commit.

--- todoapp/ViewController.swift
+++ todoapp/ViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad(){
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tableView.dataSource = self
+        tableView.dataSource = self
+        
+        
     }
 
 
