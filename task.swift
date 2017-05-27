11c72c63f04348a3c136e95168edb7adaf13e470
qamarali <qamarali@qamaralis-MacBook-Pro.local>
2017-05-27 17:29:12 -0500

    Added task class and some sample task

---
+++ task.swift
@@ -0,0 +1,15 @@
+//
+//  task.swift
+//  todoapp
+//
+//  Created by qamarali on 5/27/17.
+//  Copyright © 2017 qamarali. All rights reserved.
+//
+
+import Foundation
+
+class Task{
+
+    var name = ""
+    var important = false
+}
--- todoapp/ViewController.swift
+++ todoapp/ViewController.swift
@@ -8,16 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegate {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+
 
     @IBOutlet weak var tableView: UITableView!
-
+    
+    var tasks:[Task] = []
+    
     override func viewDidLoad(){
         super.viewDidLoad()
-        tableView.dataSource = self
-        tableView.dataSource = self
         
+        tasks = makeTask()
+        tableView.dataSource = self
+        tableView.delegate = self
+    
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important{
+            cell.textLabel?.text = "!\(task.name)"
+        }
+        else {
+        cell.textLabel?.text = task.name
+        }
+        
+        return cell
+    }
+    
+    func makeTask() -> [Task] {
+        
+        let task = Task()
+        task.important = true
+        task.name = "Roza"
+        
+        let task1 = Task()
+        task1.important = true
+        task1.name = "Namaz"
+        
+        let task2 = Task()
+        task2.important = true
+        task2.name = "Quran"
+        
+        let task3 = Task()
+        task3.important = false
+        task3.name = "timepass"
+
+        
+        return [task,task1,task2,task3]
     }
 
 
