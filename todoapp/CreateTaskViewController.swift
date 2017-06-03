6a1b130ac5e9b1f10bf661738dca8971fc3610bd
qamarali <qamarali@qamaralis-MacBook-Pro.local>
2017-06-03 18:47:30 -0500

    Added new class

---
+++ todoapp/CreateTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateTaskViewController.swift
+//  todoapp
+//
+//  Created by qamarali on 5/28/17.
+//  Copyright © 2017 qamarali. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+   
+    @IBOutlet weak var ImporantantSwitch: UISwitch!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func AddToDo(_ sender: Any) {
+        	
+    }
+}
